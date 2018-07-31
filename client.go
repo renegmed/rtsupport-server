eb6eefef756fc322fdea53541c2afd4ecd8eb48a
rene <renegmed@yahoo.com>
2018-07-31 10:57:12 -0400

    04 created router and adjusted client

--- client.go
+++ client.go
@@ -1,55 +1,44 @@
 package main
 
 import (
-	"fmt"
-	"math/rand"
-	"time"
+	"github.com/gorilla/websocket"
 )
 
-type Message struct {
-	Name string      `json:"name"`
-	Data interface{} `json:"data"`
-}
+type FindHandler func(string) (Handler, bool)
 
 type Client struct {
-	send chan Message
+	send        chan Message
+	socket      *websocket.Conn
+	findHandler FindHandler
 }
 
-func (client *Client) write() {
+func (client *Client) Read() {
+	var message Message
+	for {
+		if err := client.socket.ReadJSON(&message); err != nil {
+			break
+		}
+		// what function to call?
+		if handler, found := client.findHandler(message.Name); found {
+			handler(client, message.Data)
+		}
+	}
+	client.socket.Close()
+}
+
+func (client *Client) Write() {
 	for msg := range client.send {
-		fmt.Printf("%#v\n", msg)
+		if err := client.socket.WriteJSON(msg); err != nil {
+			break
+		}
 	}
+	client.socket.Close()
 }
 
-func (client *Client) subscribeChannels() {
-	// TODO: changefeed Query RethinkDB
-	for {
-		time.Sleep(r())
-		client.send <- Message{"channel add", ""}
-	}
-}
-
-func (client *Client) subscribeMessages() {
-	// TODO: changefeed Query RethinkDB
-	for {
-		time.Sleep(r())
-		client.send <- Message{"message add", ""}
-	}
-}
-
-func r() time.Duration {
-	return time.Millisecond * time.Duration(rand.Intn(1000))
-}
-
-func NewClient() *Client {
+func NewClient(socket *websocket.Conn, findHandler FindHandler) *Client {
 	return &Client{
-		send: make(chan Message),
+		send:        make(chan Message),
+		socket:      socket,
+		findHandler: findHandler,
 	}
-}
-
-func main() {
-	client := NewClient()
-	go client.subscribeChannels()
-	go client.subscribeMessages()
-	client.write()
 }
--- main.go
+++ main.go
@@ -1,13 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-
-	"time"
-
-	"github.com/gorilla/websocket"
-	"github.com/mitchellh/mapstructure"
 )
 
 type Message struct {
@@ -20,73 +14,12 @@ type Channel struct {
 	Name string `json:"name"`
 }
 
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+
+	router := NewRouter()
+
+	//router.Handle("channel add", addChannel)
+
+	http.Handle("/", router)
 	http.ListenAndServe(":4000", nil)
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
-	//fmt.Fprintf(w, "Hello from go")
-	socket, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println("entering for.....")
-	for {
-
-		var inMessage Message
-		var outMessage Message
-		if err := socket.ReadJSON(&inMessage); err != nil {
-			fmt.Println("--- Error 1 ----")
-			fmt.Println(err)
-			break
-		}
-
-		fmt.Printf("%#v\n", inMessage)
-		switch inMessage.Name {
-		case "channel add":
-			err := addChannel(inMessage.Data)
-			if err != nil {
-				outMessage = Message{"error", err}
-				if err := socket.WriteJSON(outMessage); err != nil {
-					fmt.Println("--- Error 2 ----")
-					fmt.Println(err)
-					break
-				}
-			}
-		case "channel subscribe":
-			go subscribeChannel(socket)
-		}
-	}
-}
-
-func addChannel(data interface{}) error {
-	var channel Channel
-
-	err := mapstructure.Decode(data, &channel)
-	if err != nil {
-		return err
-	}
-	channel.Id = "1"
-	//fmt.Printf("%#v\n", channel)
-	fmt.Println("added channel")
-	return nil
-}
-
-func subscribeChannel(socket *websocket.Conn) {
-	// TODO: rethinkDB Query / changefeed
-	for {
-		time.Sleep(time.Second * 1)
-		message := Message{"channel add",
-			Channel{"1", "Software Support"}}
-		socket.WriteJSON(message)
-		fmt.Println("sent new channel")
-	}
 }
